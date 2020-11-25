37cfee479c67081099a8d93a79bdb2cbd8279602
alpin123456 <75006139+alpin123456@users.noreply.github.com>
2020-11-25 12:58:32 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,156 @@
+
+<html> 
+
+<head> 
+
+<title> Hacked By THIS IS ALPIN</title> 
+
+<link rel="icon" type="image/jpg" href="https://e.top4top.io/p_16384mwfa0.jpeg">
+
+     <meta name="description" content="Can you love me?">
+
+     <meta name="keyword" content="Do You ready tolose">
+
+     <script>alert("THIS IS ALPIN !!!");</script>
+
+     <script>alert("I love my life because my life is you !");</script>
+
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/daun-berguguran.js" type"text/javascript"></script> 
+
+<style> 
+
+       @import url('https://fonts.googleapis.com/css?family=Iceland');
+
+       html, body { 
+
+              color: black; 
+
+              font-family:Iceland; 
+
+              font-weight: 80; 
+
+              height: 87vh; 
+
+              margin: 0;
+
+               }
+
+      .full-height { height: 87vh; }
+
+      .flex-center { align-items: center; display: flex; justify-content: center; }
+
+      .position-ref { position: relative; }
+
+      .content { text-align: center; }
+
+      .title { font-size: 36px; padding: 20px; } 
+
+      display:none }
+
+      
+
+       h1,h2,p,a{
+
+       font-family: Kelly Slab;
+
+       font-weight: normal;
+
+       }
+
+       
+
+       .jam-digital-x {
+
+       overflow: hidden;
+
+       width: 330px;
+
+       margin: 20px auto;
+
+       border: 10px solid #cyan;
+
+       }
+
+       .kotak{
+
+       float: left;
+
+       width: 100px;
+
+       height: 100px;
+
+       background-color: #cyan;
+
+       }
+
+       .jam-digital-x p {
+
+       color: red;
+
+       font-size: 40px;
+
+       text-align: center;
+
+       margin-top: 30px;
+
+       
+
+       }       
+
+ </style> 
+
+ </head> 
+
+ <body align="center" oncontextmenu="return false" bgcolor="white"> 
+
+ <div class="stars"><div class="twinkling"><div class="flex-center position-ref full-height"><div class="content"><div class="text"><b><i>
+
+ <div class="jam-digital-x"><div class="kotak"><p id="jam"></p></div><div class="kotak"><p id="menit"></p></div><div class="kotak"><p id="detik"></p></div></div>            
+
+ <br><br><img src='https://i.pinimg.com/originals/cc/74/59/cc745965068d0d497ae7662adb73bd24.png' style="width:500px;">
+
+ <br><font size="40px">Hacked By <font color="red">THIS IS ALPIN</font> 
+
+ <br><br><font size="6" color="red">=></font><font size="6"> I Always Love You in 3 Time Now, Tomorrow and Forever :) </font><font size="6" color="red"><=</font> 
+
+ <br><br><font size="6" color="red"> Thanks to : </font> <marquee direction="left" scrollamount="15" ><font size="5" color="red"> ~ </font><font size="5">GH05T666</font><font size="5" color="red"> ~ </font><font size="5">Q.GH05T666</font><font size="5" color="red"> ~ </font><font size="5">D3V0L0P3R 37</font><font size="5" color="red"> ~ </font><font size="5">Razor</font><font size="5" color="red"> ~ </font><font size="5">BL4CK-R05E</font><font size="5" color="red"> ~ </font><font size="5">./Kabur</font><font size="5" color="red"> ~ </font><font size="5">D4nzX0</font><font size="5" color="red"> ~ </font><font size="5">Rizal01</font><font size="5" color="red"> ~ </font><font size="5">"Flo</font><font size="5" color="red"> ~ </font><font size="5">SadBroder_kh4t3m</font><font size="5" color="red"> ~ </font><font size="5">Babycode</font><font size="5" color="red"> ~ </font><font size="5">Hanabi</font><font size="5" color="red"> ~ </font><font size="5" color="red">THIS IS ALPIN</font><font size="5" color="red"> ~ </font><font size="5">Grapelce</font><font size="5" color="red"> ~ </font><font size="5">YuT4_Ra</font><font size="5" color="red"> ~ </font><font size="5">TN SASORI</font><font size="5" color="red"> ~</font><font size="5" color="red"> ~</font><font size="5">KISAME </font><font size="5" color="red"> ~</font><font size="5">RZKY</font><font size="5" color="red"> ~</font></marquee>
+
+ <br><font size="6" color="red"> Message: <br></font><font size="5" color="red"style="border:1px solid; border-color:red;" width="80%"> Hargai Aku Selagi Ada ,Dan intinya Aku cinta kamu Muehehehe</font></marquee> 
+
+ <br><font size="7"></font>
+
+ </font><font size="5"><br>GHOST EXPLOITER TEAM</font>
+
+ <font size="5"><br>&copy; Copyleft 2020</font>
+
+ </div>
+
+ </div>
+
+ </div> <iframe width="0%" height="0" scrolling="no" frameborder="no" src="https://j.top4top.io/m_1636z2c2q1.mp3"></iframe>
+
+ </body> 
+
+ <script>
+
+ window.setTimeout("waktu()", 1000);
+
+ 
+
+ function waktu() {
+
+ var waktu = new Date();
+
+ setTimeout("waktu()", 1000);
+
+ document.getElementById("jam").innerHTML = waktu.getHours();
+
+ document.getElementById("menit").innerHTML = waktu.getMinutes();
+
+ document.getElementById("detik").innerHTML = waktu.getSeconds();
+
+ }
+
+ </script>
+
+ </html> 
